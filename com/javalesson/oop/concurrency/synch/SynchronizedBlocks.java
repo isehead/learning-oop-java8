dd2ee14e892b7ec209b9c1d274bd214a4d1325c9
isehead <isehead@gmail.com>
2020-02-10 23:07:30 +0500

    Intro to synchronized

--- com/javalesson/oop/concurrency/synch/SynchronizedBlocks.java
+++ com/javalesson/oop/concurrency/synch/SynchronizedBlocks.java
@@ -13,36 +13,57 @@ public class SynchronizedBlocks {
     private List<Integer> intList1 = new ArrayList<>();
     private List<Integer> intList2 = new ArrayList<>();
 
+    private final Object lock1 = new Object();
+    private final Object lock2 = new Object();
+
     public static void main(String[] args) {
         SynchronizedBlocks sb = new SynchronizedBlocks();
         sb.copy();
-        System.out.println("test");
     }
 
     private void copy() {
         long start = System.currentTimeMillis();
+        Thread thread1 = new Thread(new RunnerA());
+        Thread thread2 = new Thread(new RunnerB());
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken: " + (end - start) + " ms");
     }
 
     private void copyArrayA() {
-        for (int i = 0; i < a.length; i++) {
-            intList1.add(a[i]);
-            System.out.println(intList1);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        synchronized (lock1) {
+            for (int i = 0; i < a.length; i++) {
+                intList1.add(a[i]);
+                System.out.println(intList1);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
+
     }
 
     private void copyArrayB() {
-        for (int i = 0; i < b.length; i++) {
-            intList2.add(a[i]);
-            System.out.println(intList2);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        synchronized (lock2) {
+            for (int i = 0; i < b.length; i++) {
+                intList2.add(b[i]);
+                System.out.println(intList2);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
