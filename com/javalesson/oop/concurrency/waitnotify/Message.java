ffb4bbaf80e9d8e606ab6af9f1afc21db649ac76
isehead <isehead@gmail.com>
2020-02-13 21:47:33 +0500

    Wait-notify

---
+++ com/javalesson/oop/concurrency/waitnotify/Message.java
@@ -0,0 +1,14 @@
+package com.javalesson.oop.concurrency.waitnotify;
+
+public class Message {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
